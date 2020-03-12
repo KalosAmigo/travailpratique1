b953b2383f4bbff6da285681eaa73f3b70309efd
bob <bob@max.com>
2020-03-12 18:44:25 -0400

    bambocommit

--- TP1-Structures.cpp
+++ TP1-Structures.cpp
@@ -8,17 +8,23 @@ using namespace std;
 
 int Calcul_Diviseur(int numerateur, int denominateur);
 string Afficher_Simplifier(int numerateur, int denominateur);
+void Afficher_Final(int tabNum[], int tabDeNum[], int taille);
+void saisie(int tabNum[], int tabDeNum[], int taille);
+int verification(); 
 
 int main()
 {
-    int numerateur, denominatur;
-    cout << "Entrez numerateur: ";
-    cin >> numerateur;
-    cout << "Entrez denominateur: ";
-    cin >> denominatur;
+    int taille{};
+    cout << "Entrez le nombre de fraction : "; 
+    taille = verification();
 
-    cout << Afficher_Simplifier(numerateur, denominatur);
+    int* tabNum{ new int [taille] {} };
+    int* tabDeNum{ new int [taille] {} };
+    saisie(tabNum, tabDeNum, taille);
+    Afficher_Final(tabNum, tabDeNum, taille);
+    delete[] tabNum, tabDeNum; 
 }
+
 
 int Calcul_Diviseur(int numerateur, int denominateur) {
     int minimum = numerateur;
@@ -40,5 +46,43 @@ string Afficher_Simplifier(int numerateur, int denominateur) {
     int aff_num = numerateur / Calcul_Diviseur(numerateur, denominateur);
     int aff_den = denominateur / Calcul_Diviseur(numerateur, denominateur);
 
-    return (to_string(aff_num) + " / " + to_string(aff_den));
+    return (to_string(aff_num) + "/" + to_string(aff_den));
 }
+
+void Afficher_Final(int tabNum[], int tabDeNum[], int taille) {
+    for (int i = 0; i < taille; i++)
+    {
+        cout << tabNum[i] << "/" << tabDeNum[i] << " >>> " << Afficher_Simplifier(tabNum[i], tabDeNum[i]) << endl;  
+
+    }
+}
+
+void saisie(int tabNum[], int tabDeNum[], int taille) {
+    for (int i = 0; i < taille; i++)
+    {
+        cout << "entrez un numerateur : ";
+        tabNum[i] = verification();
+        cout << "entrez un denumerateur : ";
+        tabDeNum[i] = verification();
+
+    }
+    
+}
+
+int verification() {
+    int n;
+
+    do {
+        cin >> n;
+        if (n <= 0)
+            cout << "erreur reesayer : "; 
+    } while (n <= 0);
+    return n; 
+}
+
+
+
+
+
+
+
